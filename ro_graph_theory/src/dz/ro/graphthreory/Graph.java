d8393989764d54c7a1c863ef3dcd55be10327a19
AbdelliNasredine <abdou.nasredine@gmail.com>
2019-12-26 02:27:26 +0100

    initial implementation of Graph class

--- ro_graph_theory/src/dz/ro/graphthreory/Graph.java
+++ ro_graph_theory/src/dz/ro/graphthreory/Graph.java
@@ -1,5 +1,27 @@
 package dz.ro.graphthreory;
 
-public class Graph {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Graph {
+    private List<Sommet> X;
+    private List<Arc> U;
+
+    public Graph() {
+        this.X = new ArrayList<Sommet>();
+        this.U = new ArrayList<Arc>();
+    }
+
+    public Graph(List<Sommet> x, List<Arc> u) {
+        X = x;
+        U = u;
+    }
+
+    @Override
+    public String toString() {
+        return "Graph{" +
+                "\tX=" + X +
+                "\n\tU=" + U +
+                "\n}";
+    }
 }
