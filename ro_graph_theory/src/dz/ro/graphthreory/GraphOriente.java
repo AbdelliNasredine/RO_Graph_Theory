d1bf0a1e407265bf0cda3bacf3e939105b1ad1da
AbdelliNasredine <abdou.nasredine@gmail.com>
2019-12-27 23:58:11 +0100

    added GraphOriente.java

---
+++ ro_graph_theory/src/dz/ro/graphthreory/GraphOriente.java
@@ -0,0 +1,12 @@
+package dz.ro.graphthreory;
+
+import java.util.List;
+
+public class GraphOriente extends Graph{
+    public GraphOriente(){
+        super();
+    }
+    public GraphOriente(List<Sommet> x, List<Arc> u){
+        super(x, u);
+    }
+}
